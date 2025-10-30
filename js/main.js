fa936359882185562bb68565a220daf1d6bb34c4
YaroFTD <aroslavanciferov9@gmail.com>
2025-10-30 18:21:55 +0400

    Добавлена анимация шапки Сверстан блок about

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ let menuBody = document.querySelector('.menu__body');
 menu.addEventListener('click', function () {
    document.body.classList.toggle('menu-open');
 });
+
+// ЛИПКИЙ HEADER
+let header = document.querySelector('.header');
+
+document.onscroll = function () {
+   let scroll = window.scrollY;
+   if (scroll > 0){
+      header.classList.add('_fixed');
+   } else {
+      header.classList.remove('_fixed');
+   }
+}
